88b915872d5f9efa94a547e75a54609dc8183118
Tom Kurzeka <tomarturo@Toms-Big-Macbook-Pro.local>
2023-07-13 12:57:26 -0700

    first commit, added config.toml + gitignore, set backgroundColor to chartreuse

---
+++ streamlit-app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Here is my app")
+st.header("In config.toml my backgroundColor is set to **chartreuse**")
+st.write("The background is **chartreuse** when running on **localhost** and **streamlit cloud**")
+st.write("The background is **white** when I'm embedded as an **iframe**")
+
+
